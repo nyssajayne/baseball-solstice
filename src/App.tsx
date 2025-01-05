aa20bf0fffa3e47b373b118026a730a3a476e34b
Nyssa Tyers-McGwyre <nyssa@wunderkind.co>
2025-01-05 13:56:41 -0500

    feat: turn front page into baseball card

--- src/App.tsx
+++ src/App.tsx
@@ -47,23 +47,42 @@ function App() {
   }, []);
 
   return (
-    <div className={showTeam ? `${classes.showTeam} ${classes.wrapper}` : `${classes.wrapper}`}>
-      <header>
-        <Logo />
+    <div className={classes.card}>
+      <div className={classes.innerCard}>
+        <div className={classes.logoWrapper}>
+          <Logo />
+        </div>
         <h1 className={classes.h1}>Offseason Baseball Solstice Calculator</h1>
-        <h2  className={classes.h2}>Are you closer to your last game of baseball or your next game of baseball this off-season?<br />
-              Search for your team and find out!</h2>
-      </header>
-      
-      <Select options={selectOptions} onChange={handleSearchValue} isClearable={true} />
+        <div className={classes.h2Wrapper}>
+          <h2 className={classes.h2}>Are you closer to your last game of baseball or your next game of baseball this off-season?<br />
+               Search for your team and find out!</h2>
+        </div>
 
-      {showTeam && <Diamond key={`diamond-${showTeam.id}`} solstice={showTeam}/>}
-
-      <div className={showTeam ? `${classes.showTeam} ${classes.explainer}` : `${classes.explainer}`}>
-        <h2>What is the Offseason Baseball Solstice?</h2>
-        <p>During the regular season, there are certain markers that let you know where you are, like the All-Star Break and the trading deadline.  However, during the off-season, there are no such markers, it's just a cold winter and maybe a countdown until opening day.  The Offseason Baseball Solstice chart helps you visiualise how far into the offseason you are.</p>
+        <Select 
+          className={classes.select}
+          isClearable={true} 
+          menuPlacement='top'
+          onChange={handleSearchValue}
+          options={selectOptions} />
       </div>
     </div>
+    // <div className={showTeam ? `${classes.showTeam} ${classes.wrapper}` : `${classes.wrapper}`}>
+    //   <header>
+    //     <Logo />
+    //     <h1 className={classes.h1}>Offseason Baseball Solstice Calculator</h1>
+    //     <h2  className={classes.h2}>Are you closer to your last game of baseball or your next game of baseball this off-season?<br />
+    //           Search for your team and find out!</h2>
+    //   </header>
+      
+    //   <Select options={selectOptions} onChange={handleSearchValue} isClearable={true} />
+
+    //   {showTeam && <Diamond key={`diamond-${showTeam.id}`} solstice={showTeam}/>}
+
+    //   <div className={showTeam ? `${classes.showTeam} ${classes.explainer}` : `${classes.explainer}`}>
+    //     <h2>What is the Offseason Baseball Solstice?</h2>
+    //     <p>During the regular season, there are certain markers that let you know where you are, like the All-Star Break and the trading deadline.  However, during the off-season, there are no such markers, it's just a cold winter and maybe a countdown until opening day.  The Offseason Baseball Solstice chart helps you visiualise how far into the offseason you are.</p>
+    //   </div>
+    // </div>
   )
 }
 
