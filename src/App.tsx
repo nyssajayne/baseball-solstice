25242d0f396609e747e7f56e7e9e88dea30396b6
Nyssa Tyers-McGwyre <nyssa@wunderkind.co>
2025-01-11 14:27:23 -0500

    fix: unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { act, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import classes from './App.module.css';
 import Logo from './components/Logo';
 import Diamond from './components/Diamond';
